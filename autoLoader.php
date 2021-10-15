7894b3869a96ffa4a261a543233d923510a55997
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2021-10-15 00:55:18 -0700

    save

--- autoLoader.php
+++ autoLoader.php
@@ -2,7 +2,17 @@
 
 session_start();
 
-spl_autoload_register(function($class){
+require_once './business/LoginService.php';
+require_once './business/RegistrationService.php';
+require_once './business/SecretsService.php';
+require_once './business/models/KVPair.php';
+require_once './business/models/Secret.php';
+require_once './database/database.php';
+require_once './database/loginDAO.php';
+require_once './database/registrationDAO.php';
+require_once './database/secretsDAO.php';
+
+/*spl_autoload_register(function($class){
     
     $lastDirectories = substr(getcwd(), strlen(__DIR__));
     
@@ -24,5 +34,5 @@ spl_autoload_register(function($class){
             }
         }
     }
-});
+});*/
 ?>
