9e328a09deb9305365632f7c63013b770f048ab9
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2021-10-14 22:11:57 -0700

    Updated MySQL connections

--- database/database.php
+++ database/database.php
@@ -1,10 +1,10 @@
 <?php
 
 class Database {
-    private $server = "localhost";
-    private $user = "root";
-    private $password = "root";
-    private $database = "secretsmanager";
+    private $server = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    private $user = "d8jpt1j5l95dqdc8";
+    private $password = "c2fr7zshqy4hq7oa";
+    private $database = "wbupv9ogyasn9zg1";
     private $port = "3306";
     
     public function getConnection(){
