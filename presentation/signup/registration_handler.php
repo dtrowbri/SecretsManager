b92586b7e47b089dacab8883e01c263d8efb8825
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2021-10-15 00:50:20 -0700

    save

--- presentation/signup/registration_handler.php
+++ presentation/signup/registration_handler.php
@@ -4,24 +4,16 @@
 <body>
 <?php 
 require_once '../../_header.php';
-echo "<p>adding autoloader</p>";
 //require_once '../../autoLoader.php';
 require_once '../../business/RegistrationService.php';
-echo "reg service added";
 require_once '../../database/registrationDAO.php';
-echo "reg dao added";
 require_once '../../database/database.php';
-echo "database added";
 
-echo "session status: " . session_status();
 $login = $_POST["Login"];
-echo "first post";
 $password = $_POST["password"];
 $passwordVerification = $_POST["passwordVerification"];
 
-echo "creating service";
 $registrationService = new RegistrationService();
-echo "serivce created";
 if($registrationService->doesLoginExist($login)){
     echo "The login " . $login . " is already in use, please use a different login";
 } else {
