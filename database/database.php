0ab9fce6ade8a378d71010b0954dae640f78dcaa
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2021-10-30 18:42:25 -0700

    save

--- database/database.php
+++ database/database.php
@@ -6,10 +6,11 @@ class Database {
     private $password = "Thisisnotagoodpassword1234!#$";
     private $database = "secretsmanager";
     private $port = "3306";
-    
+
     public function getConnection(){
-        $conn = new mysqli($this->server, $this->user, $this->password, $this->database, $this->port);
-        
+        // $conn = new mysqli($this->server, $this->user, $this->password, $this->database, $this->port);
+        $conn = new mysql(null, $this->user, $this->password, $this->database, null, '/cloudsql/sacred-brace-330505:us-west1:secretsmanagerdb');
+
         if($conn->connect_error){
             echo "Connection failed " . $conn->connect_error;
             return null;
@@ -17,6 +18,6 @@ class Database {
             return $conn;
         }
     }
-    
+
 }
 ?>
