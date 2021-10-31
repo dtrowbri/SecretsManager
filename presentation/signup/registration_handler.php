026b8082e77c59ad1879d8088f4e9746d042a309
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2021-10-30 22:17:00 -0700

    save

--- presentation/signup/registration_handler.php
+++ presentation/signup/registration_handler.php
@@ -9,9 +9,8 @@ $login = $_POST["Login"];
 $password = $_POST["password"];
 $passwordVerification = $_POST["passwordVerification"];
 
-echo "creating registration service";
+
 $registrationService = new RegistrationService();
-echo "registration service created";
 if($registrationService->doesLoginExist($login)){
     echo "The login " . $login . " is already in use, please use a different login";
 } else {
@@ -20,9 +19,7 @@ if($registrationService->doesLoginExist($login)){
     } else {
         $salt = "salt";
         $passwordHash = hash("sha512", $salt . $password);
-        echo "registering new user";
         $isSuccessful = $registrationService->registerNewUser($login, $passwordHash);
-        echo "new user registered";
         if($isSuccessful){
             echo '<div class="container">Your login has been successfully created.</div>';
         } else {
