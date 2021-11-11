47ab5674639f073241326eff4ee8947c2670ecb2
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2021-11-11 00:27:34 -0700

    Update registrationDAO.php
    save

--- database/registrationDAO.php
+++ database/registrationDAO.php
@@ -9,7 +9,7 @@ class RegistrationDAO {
     
     public function __construct(){
         $this->logger = new Logger('main');
-        $this->logger->pushHandler( new StreamHandler('php://stderr', Logger::DEBUG));
+        $this->logger->pushHandler( new StreamHandler('php://stdout', Logger::DEBUG));
         $this->logger->debug("Creating RegistrationDAO", ['session' => session_id(), 'class' => 'RegistrationDAO', 'method' => 'construct']);
     }
     
