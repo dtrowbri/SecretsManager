757264398e9aaff50966f0779fbf79debcecd9c0
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2021-11-11 00:05:32 -0700

    Update loginhandler.php
    save

--- presentation/login/loginhandler.php
+++ presentation/login/loginhandler.php
@@ -10,7 +10,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 $logger = new Logger('main');
-$logger->pushHandler( new StreamHandler('php://stderr', Logger::DEBUG));
+$logger->pushHandler( new StreamHandler('php://stdout', Logger::DEBUG));
 
 $login = $_POST["Login"];
 $password = $_POST["Password"];
