cbf9ea3421953ece6eb32b27929efa17eb9c0da3
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2021-11-10 23:32:56 -0700

    Update autoLoader.php
    save

--- autoLoader.php
+++ autoLoader.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-spl_autoload_register(function($class){
+/*spl_autoload_register(function($class){
     
     $lastDirectories = substr(getcwd(), strlen(__DIR__));
     
@@ -24,7 +24,16 @@ spl_autoload_register(function($class){
             }
         }
     }
-});
+});*/
 
-    require_once __DIR__ .'/vendor/autoload.php';
+require_once 'business/LoginService.php';
+require_once 'business/RegistrationService.php';
+require_once 'business/SecretsService.php';
+require_once 'business/models/KVPair.php';
+require_once 'business/models/Secret.php';
+require_once 'database/database.php';
+require_once 'database/loginDAO.php';
+require_once 'database/registrationDAO.php';
+require_once 'database/secretsDAO.php';
+require_once __DIR__ .'/vendor/autoload.php';
 ?>
