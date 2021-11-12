f34bd409fd5c5419e22f5d9bdbb6390f714d0c2c
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2021-11-11 20:44:29 -0700

    Update database.php
    save

--- database/database.php
+++ database/database.php
@@ -4,11 +4,11 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 class Database {
-    private $server = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    private $user = "d8jpt1j5l95dqdc8";
-    private $password = "c2fr7zshqy4hq7oa";
-    private $database = "wbupv9ogyasn9zg1";
-    private $port = 3306;
+    private $server = "34.127.126.35";
+    private $user = "admin";
+    private $password = "Thisisnotagoodpassword1234!#$";
+    private $database = "secretsmanager";
+    private $port = "3306";
     private $logger = null;
     
     public function __construct(){
@@ -19,9 +19,8 @@ class Database {
     
     
     public function getConnection(){
-        $conn = new mysqli($this->server, $this->user, $this->password, $this->database, $this->port);
-        //$conn = new mysqli(null, 'admin', 'Thisisnotagoodpassword1234!#$', 'secretsmanager', 3306, '/cloudsql/sacred-brace-330505:us-west1:secretsmanagerdb');
-        //$conn = new mysqli('34.127.126.35', 'admin', 'Thisisnotagoodpassword1234!#$', 'secretsmanager', 3306);//, '/cloudsql/sacred-brace-330505:us-west1:secretsmanagerdb');
+        $conn = new mysqli(null, $this->user, $this->password, $this->database, null, '/cloudsql/sacred-brace-330505:us-west1:secretsmanagerdb');
+        
         if($conn->connect_error){
             $this->logger->critical("Cannot connect to database server", ['session' => session_id(), 'class' => 'Database', 'method' => 'construct']);
             return null;
